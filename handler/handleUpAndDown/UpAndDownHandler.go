1c2f6fe225fd394f990a55dd3f5ff028c65eb215
pantao <pantao@flytone.cn>
2019-01-28 10:11:32 +0800

    code upload

---
+++ handler/handleUpAndDown/UpAndDownHandler.go
@@ -0,0 +1,53 @@
+package handleUpAndDown
+
+import (
+    "ai_local/hardWare/rs485/rs485Helper"
+    "github.com/astaxie/beego"
+    "github.com/tarm/serial"
+    "time"
+)
+
+var isUp = false
+var port *serial.Port
+
+func init() {
+    comName := "/dev/ttyS0"
+    baud := 38400
+    config := serial.Config{Name: comName, Baud: baud}
+    p, e := serial.OpenPort(&config)
+    for e != nil {
+        beego.Error(e.Error())
+        time.Sleep(time.Duration(5) * time.Second)
+        p, e = serial.OpenPort(&config)
+    }
+    port = p
+}
+
+func UpOrDown() error {
+    downStr := "9a:00:01:00:0a:ee:e5" //全下
+    upStr := "9a:00:01:00:0a:dd:d6"   //全上
+
+    if isUp {
+        bytes, e := rs485Helper.GetCommandBytesFromStr(downStr)
+        if e != nil {
+            return e
+        }
+        _, e = port.Write(bytes)
+        if e != nil {
+            return e
+        }
+        isUp = false
+    } else {
+        bytes, e := rs485Helper.GetCommandBytesFromStr(upStr)
+        if e != nil {
+            return e
+        }
+        _, e = port.Write(bytes)
+        if e != nil {
+            return e
+        }
+        isUp = true
+    }
+
+    return nil
+}
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/toolbox"
+	"up_and_down/handler/handleUpAndDown"
+	_ "up_and_down/routers"
+)
+
+func main() {
+	// 定时回收订单群聊
+	autoUpAndDown := toolbox.NewTask("auto_up_and_down", "0 0 * * * *", func() error {
+		e := handleUpAndDown.UpOrDown()
+		if e != nil {
+			beego.Error(e)
+		}
+		return nil
+	})
+
+	// 定时任务组建初始化
+	toolbox.AddTask("auto_up_and_down", autoUpAndDown)
+	toolbox.StartTask()
+	beego.Run()
+}
+
