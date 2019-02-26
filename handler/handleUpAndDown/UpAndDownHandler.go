45b3f9204f459f42548efe59291912ccef77bd33
loki <442186219@qq.com>
2019-02-26 11:07:26 +0800

    查询重试

--- handler/handleUpAndDown/UpAndDownHandler.go
+++ handler/handleUpAndDown/UpAndDownHandler.go
@@ -4,8 +4,8 @@ import (
 	"ai_local/hardWare/rs485/rs485Constants"
 	"ai_local/hardWare/rs485/rs485Helper"
 	"encoding/hex"
-	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/pkg/errors"
 	"github.com/tarm/serial"
 	"time"
 )
@@ -15,7 +15,7 @@ var Rs485Port *serial.Port
 func InitRs485Port() {
 	comName := "/dev/ttyS1"
 	baud := 38400
-	config := serial.Config{Name: comName, Baud: baud}
+	config := serial.Config{Name: comName, Baud: baud, ReadTimeout: time.Duration(2) * time.Second}
 	beego.Info("begin open Rs485Port")
 	p, e := serial.OpenPort(&config)
 	if e != nil {
@@ -24,36 +24,36 @@ func InitRs485Port() {
 		p, e = serial.OpenPort(&config)
 	}
 	Rs485Port = p
-	go ReadFromPort()
 }
 
-func ReadFromPort() {
-	for {
-		if Rs485Port == nil {
-			time.Sleep(time.Duration(5) * time.Second)
-			continue
-		}
-		var b = make([]byte, 1024)
-		i, e := Rs485Port.Read(b)
-		if e != nil {
-			beego.Error(e)
-			time.Sleep(time.Duration(5) * time.Second)
-			continue
-		}
-		beego.Debug(fmt.Sprintf("电机状态响应：%v", hex.EncodeToString(b[:i])))
+func ReadFromPort() (result string, err error) {
+
+	if Rs485Port == nil {
+		err = errors.New("nil Rs485Port")
+		return
 	}
+
+	var rb []byte
+	var b = make([]byte, 1024)
+	var i = 1
+	var e error
+	for i > 0 {
+		i, e = Rs485Port.Read(b)
+		if e != nil {
+			err = e
+			return
+		}
+		rb = append(rb, b[:i]...)
+	}
+
+	return hex.EncodeToString(rb), nil
 }
 
 func UpOrDown() error {
-
 	downE := AllDown()
 	if downE != nil {
 		beego.Error(downE)
 	}
-	go func() {
-		time.Sleep(time.Duration(5) * time.Second)
-		GetStatus()
-	}()
 
 	time.Sleep(time.Duration(1) * time.Minute)
 
@@ -61,15 +61,23 @@ func UpOrDown() error {
 	if upE != nil {
 		beego.Error(upE)
 	}
-	go func() {
-		time.Sleep(time.Duration(5) * time.Second)
-		GetStatus()
-	}()
 
 	return nil
 }
 
 func AllUp() error {
+
+	var tryTimes = 0
+	var result string
+	var err error
+	result, err = GetStatus()
+	for err != nil && tryTimes < 3 {
+		result, err = GetStatus()
+		tryTimes++
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+	beego.Debug("result ", result)
+
 	upStr := "9a:00:01:00:0a:dd:d6"
 	beego.Debug("全上")
 	bytes, e := rs485Helper.GetCommandBytesFromStr(upStr)
@@ -86,6 +94,18 @@ func AllUp() error {
 }
 
 func AllDown() error {
+
+	var tryTimes = 0
+	var result string
+	var err error
+	result, err = GetStatus()
+	for err != nil && tryTimes < 3 {
+		result, err = GetStatus()
+		tryTimes++
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+	beego.Debug("result ", result)
+
 	downStr := "9a:00:01:00:0a:ee:e5" //全下
 	beego.Debug("全下")
 	bytes, e := rs485Helper.GetCommandBytesFromStr(downStr)
@@ -101,17 +121,21 @@ func AllDown() error {
 	return nil
 }
 
-func GetStatus() error {
+func GetStatus() (result string, err error) {
 	if Rs485Port == nil {
-		return nil
+		err = errors.New("nil Rs485Port")
+		return
 	}
 	bytes, e := rs485Helper.GetCommandBytesFromStr(rs485Constants.QueryAll)
 	if e != nil {
-		return e
+		err = e
+		return
 	}
 	_, e = Rs485Port.Write(bytes) //上
 	if e != nil {
-		return e
+		err = e
+		return
 	}
-	return nil
+
+	return ReadFromPort()
 }
